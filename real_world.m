5844381aab360327ca43a3c34c52c983d2d72d5f
conorpeterson23 <68404881+conorpeterson23@users.noreply.github.com>
2022-04-19 09:43:46 -0400

    Update real_world.m
    Working not using target

--- real_world.m
+++ real_world.m
@@ -16,9 +16,9 @@ target      = 0.5;   % Desired height of the ball [m]
 sample_rate = 0.25;  % Amount of time between controll actions [s]
 
 %% Give an initial burst to lift ball and keep in air
-set_pwm(s, 0); % Initial burst to pick up ball
+set_pwm(s, 4000); % Initial burst to pick up ball
 pause(0.1) % Wait 0.1 seconds
-set_pwm(s, 0); % Set to lesser value to level out somewhere in
+%set_pwm(s, 0); % Set to lesser value to level out somewhere in
 % the pipe
 
 %% Initialize variables
@@ -28,26 +28,28 @@ error_sum   = 0;
 
 %% Feedback loop
 
-temp_pwm = 4000;
+% temp_pwm = 4000;
 while true
     %% Read current height
-    [distance,pwm,target,deadpan] = read_data(s)
-    distance
-    y = ir2y(distance); % Convert from IR reading to distance from bottom [m]
-    if(temp_pwm <0)
-        break;
-    end
+     [distance,pwm,target,deadpan] = read_data(s);
+     y = ir2y(distance); % Convert from IR reading to distance from bottom [m]
+     [A,B] = findab(pwm);
+     Q = [1000,100];
+     Q = diag(Q);
+     R = 1;
+     k = lqr(A, B, Q, R);
 
-    set_pwm(s, temp_pwm);
-    temp_pwm = temp_pwm - 10;
-    temp_pwm
+     pwm = -k*pwm + 2727.0447;
+
+     set_pwm(s, pwm)
+
     %% Calculate errors for PID controller
-    error_prev = error;             % D
-    error      = target - y;        % P
-    error_sum  = error + error_sum; % I
+%     error_prev = error;             % D
+%     error      = target - y;        % P
+%     error_sum  = error + error_sum; % I
     
     %% Control
-    prev_action = action;
+    %prev_action = action;
     %action = % Come up with a scheme no answer is right but do something
     %set_pwm(add_proper_args); % Implement action
         
