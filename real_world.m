1cc165fe2d5702c958cf380d8f447e04c7b501f5
SanjayThallam <60016462+SanjayThallam@users.noreply.github.com>
2022-04-23 17:34:27 -0400

    Commented Real World

--- real_world.m
+++ real_world.m
@@ -3,56 +3,49 @@
 % The current control system is a PID controller.
 %
 % Created by Kyle Naddeo, Mon Jan 3 11:19:49 EST 
-% Modified by Sanjay THallam AND 2/8/2022
+% Modified by Sanjay Thallam Conor Peterson AND Michael Mulvihill. 2/8/2022
 
 %% Start fresh
-close all; clc; clear device;
+close all; clc; clear device; % this is used to reset the the device ports
 
 %% Connect to device
 % device = open serial communication in the proper COM port
-s = serialport('COM4',19200);
+device = serialport('COM6',19200); %connecting to the com port associated with ones computer
+
+pwm = 0;
 %% Parameters
-target      = 0.5;   % Desired height of the ball [m]
-sample_rate = 0.25;  % Amount of time between controll actions [s]
+target      = 0.8;   % Desired height of the ball [m]
+sample_rate = 1;  % Amount of time between controll actions [s]
 
 %% Give an initial burst to lift ball and keep in air
-set_pwm(s, 4000); % Initial burst to pick up ball
-pause(0.1) % Wait 0.1 seconds
+set_pwm(device, 4000); % Initial burst to pick up ball
+pause(2) % Wait 0.1 seconds
+[ir, ~, ~, ~]=read_data(device);
 %set_pwm(s, 0); % Set to lesser value to level out somewhere in
 % the pipe
 
 %% Initialize variables
-action      = 3500; % Same value of last set_pwm   
-error       = 0;
-error_sum   = 0;
-
+action      = 4000; % Same value of last set_pwm   
 %% Feedback loop
 
-% temp_pwm = 4000;
 while true
-    %% Read current height
-     [distance,pwm,target,deadpan] = read_data(s);
-     y = ir2y(distance); % Convert from IR reading to distance from bottom [m]
-     [A,B] = findab(pwm);
-     Q = [1000,100];
-     Q = diag(Q);
-     R = 1;
-     k = lqr(A, B, Q, R);
+    %% Read current height 
+    [ir, ~, ~, ~] = read_data(device); % get the IR reading from the sensor
+    [y,~] = ir2y(ir); % Convert from IR reading to distance from bottom [m]
+    error = target - y %calculate the error in the ball position
 
-     pwm = -k*pwm + 2727.0447;
+    [A, B, C, D]=findab(pwm-2727); %creat hte 
+    %u is pwm
+    x=C\(y-D*pwm-2727) %this is a calcualtion for the LQR we attempted to implement
 
-     set_pwm(s, pwm)
+     
+    Q = [100,1]; %Q cost matrix (Time effort)
+    Q = diag(Q); %Make the cost matrix into a diagonal array
+    R = 1; %Cost for the R (Actuator effort)
+    K = lqr(A, B, Q, R) %Run the LQR function
 
-    %% Calculate errors for PID controller
-%     error_prev = error;             % D
-%     error      = target - y;        % P
-%     error_sum  = error + error_sum; % I
-    
-    %% Control
-    %prev_action = action;
-    %action = % Come up with a scheme no answer is right but do something
-    %set_pwm(add_proper_args); % Implement action
-        
-    % Wait for next sample
-    pause(sample_rate)
+
+    pwm = min(4095,max(K(1),1000)); %figure out what pwm the fan will be set to
+    action=set_pwm(device, pwm) %Send the new PWM value to the system
+    pause(sample_rate); %wait until the next iteration.
 end
