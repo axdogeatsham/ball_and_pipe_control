50c02ac341db432b729c04ac6ee0d5f4053f798e
SanjayThallam <60016462+SanjayThallam@users.noreply.github.com>
2022-04-23 17:48:52 -0400

    Commented set pwm

--- set_pwm.m
+++ set_pwm.m
@@ -1,4 +1,4 @@
-function action = set_pwm(device, pwm_value)
+function [action] = set_pwm(device, pwm_value)
 %% Sets the PWM of the fan
 % Inputs:
 %  ~ device: serialport object controlling the real world system
@@ -8,15 +8,17 @@ function action = set_pwm(device, pwm_value)
 %  ~ action: the control action to change the PWM
 %
 % Created by:  Kyle Naddeo 1/3/2022
-% Modified by: YOUR NAME and DATE
+% Modified by: Conor Peterson, Sanjay Thallam and Mike Mulvihill
 
 %% Force PWM value to be valid
 % pwm_value = [0 4095] % Bound value to limits 0 to 4095
 
 %% Send Command
-x = max(min(pwm_value, 4095),0);
-action = strcat("P", sprintf("%04.f",x)) % string value of pwm_value
-write(device, action, "string");
+bounded = max(min(pwm_value, 4095),0); %bound the pwm to the limits of the fan
+pwm_value; %print pwm value (for debug)
+bounded; %print bounded pwm value (for debug)
+action = strcat("P", sprintf("%04.f",bounded(1))); % string value of pwm_value
+write(device, action, "string"); %write the pwm to the fan
 % use the serialport() command options to change the PWM value to action
 
 end
