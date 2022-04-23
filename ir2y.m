cace01fdec78129243800ace670a59476810b5ef
SanjayThallam <60016462+SanjayThallam@users.noreply.github.com>
2022-04-23 17:40:32 -0400

    Commented ir2y

--- ir2y.m
+++ ir2y.m
@@ -8,17 +8,18 @@ function [y, pipe_percentage] = ir2y(ir)
 %  where is the ball
 %
 % Created by:  Kyle Naddeo 1/3/2022
-% Modified by: Conor Peterson and 2/15/22
+% Modified by: Conor Peterson, Mike Mulvihill and Sanjay Thallam 2/15/22
 
 %% Parameters
-ir_bottom = 956  % IR reading when ball is at bottom of pipe
-ir_top    = 60  % "                        " top of pipe
+ir_bottom = 956;  % IR reading when ball is at bottom of pipe
+ir_top    = 60;  % IR reading when ball is at top of pipe
 y_top     = 0.9144; % Ball at top of the pipe [m]
 
 %% Bound the IR reading and send error message 
 % (remeber the IR values are inverted ie small values == large height and large values == small height)
 
 %% Set
-pipe_percentage = 50
-y = 0.457
-
+ir;
+irBound = max(min(ir, 956), 60); %Set the IR Bounds
+pipe_percentage = 1-((irBound-ir_top)/(ir_bottom-ir_top)); %The percent that the ball is from the bottom of the pipe
+y = y_top*pipe_percentage; %find the y height in meters
